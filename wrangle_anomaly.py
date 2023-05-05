c501a53ab8e1ba146434c6a534d808d8fb9dd15b
Adam Harris <afharris87@gmail.com>
2023-05-05 15:14:12 -0500

    working on time series anomalies

--- wrangle_anomaly.py
+++ wrangle_anomaly.py
@@ -3,23 +3,44 @@ import numpy as np
 import pandas as pd
 # import file access tools
 import os
+import env
 
 # -----------------------------------------------
 
 def acquire_web_traffic():
     '''
-    this will read in the codeup web traffic log from txt file in the local directory
+    this will read in the codeup web traffic log from csv file in the local directory,
+    if one does not exist, it will download the data via sql from the codeup server
     '''
     # set file name
-    filename = 'anonymized-curriculum-access.txt'
+    filename = 'curriculum_logs.csv'
     # check if file exists in local directory
     if os.path.exists(filename):
+        print('opening file from local directory')
         # read in the file to a dataframe
-        traffic = pd.read_csv(filename, sep=' ', header=None)
+        traffic = pd.read_csv(filename, index_col=0)
     # if the file doesn't exist
     else:
         # print an error
-        print(f'file {filename} not found in local directory')
+        print(f'file {filename} not found in local directory, downloading via SQL')
+        # download dataset from codeup server
+        # set database we are looking at
+        database = 'curriculum_logs'
+        # create sql connectioni
+        connection = env.get_db_url(database)
+        # create sql query
+        query = '''
+select l.date, l.time, l.path, l.user_id, l.cohort_id as cohort_name,
+	l.ip, c.name, c.start_date, c.end_date, c.program_id
+from logs l
+join cohorts c
+	on l.cohort_id = c.id
+;
+        '''
+        # query server via sql
+        traffic = pd.read_sql(query, connection)
+        # cache the data to local csv file
+        traffic.to_csv(filename)
     # return the dataframe
     return traffic
 
@@ -29,15 +50,25 @@ def prepare_web_traffic(traffic):
     '''
     this will prepare the codeup web traffic dataframe for use
     '''
-    # set column names
-    traffic = traffic.rename(columns={
-        0:'date', 1:'time', 2:'page', 3:'user_id', 4:'cohort', 5:'ip'})
     # combine the date and time into one varaible
     traffic['datetime'] = traffic.date + ' ' + traffic.time
     # change the new datetime column to datetime type
     traffic.datetime = pd.to_datetime(traffic.datetime)
     # drop the redundant date and time columns and set datetime as the index
     traffic = traffic.drop(columns=['date', 'time']).set_index('datetime')
+    # drop null rows (there's only 1)
+    traffic = traffic.dropna()
+    # convert first part of website path into column called lesson
+    # create an empty list
+    page_list=[]
+    # split the webpage path by '/'
+    pages = traffic.path.str.split('/')
+    # go through all of the pages
+    for i in range(len(pages)):
+        # make a list of the first part of the paths
+        page_list.append(pages[i][0])
+    # create a new column with the first part of the path
+    traffic['lesson'] = page_list
     # return the prepared dataframe
     return traffic
 
